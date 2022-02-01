3b95af9ea8f40ab683698c1305fdf8d18b51904a
EmmaClaire <emma.kinnison@marquette.edu>
2022-02-01 14:35:09 -0600

    This is initiaitlizing my files, copied inclass example code inside as well

---
+++ Lab1/kininsone_stats.hpp
@@ -0,0 +1,33 @@
+/*Name: Emma Claire Kinnison
+Assignment: Lab 1
+Date 2/1/22
+Comments: This is the c file to sum the random numbers and output the resutle
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace mySpace{
+    namespace yourSpace{
+        class myClass{
+            public:
+
+                 //construct myClass with an intial name, and number of students
+                myClass(std::sting name, int init_num_students);
+                myClass)std::string name);
+
+                int get_num_students();
+                std::string get_name();
+                void set_num_students(int num);
+
+
+            private:
+                int num_students;
+                std::string className;
+
+
+        }
+    }
+}
+
---
+++ Lab1/kinnisone_stats.cpp
@@ -0,0 +1,5 @@
+/*Name: Emma Claire Kinnison
+Assignment: Lab 1
+Date 2/1/22
+Comments: This is the c file to sum the random numbers and output the resutle
+*/
