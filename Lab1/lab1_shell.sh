1b0175b5b73bdfa80eb7b646f6b8de03e6cb536a
EmmaClaire <emma.kinnison@marquette.edu>
2022-02-08 16:02:13 -0600

    Initial user input checking on shell script

--- Lab1/lab1_shell.sh
+++ Lab1/lab1_shell.sh
@@ -3,4 +3,33 @@
 ##2/8/21
 ##comments:This is my shell script for lab 1
 
+echo "Which concentration would you like? ERP00 or ERP05?"
 
+read userin
+
+
+while [ ! $userin == "ERP00" ] && [ ! $userin == "ERP05" ];
+do
+    echo "Please try again!"
+    echo "ERP00 or ERP005"
+    read userin
+done    
+
+
+
+if [ $userin == "ERP00" ];
+then
+
+
+
+
+fi
+
+
+if [ $userin == "ERP05" ];
+then
+
+
+
+    
+fi
