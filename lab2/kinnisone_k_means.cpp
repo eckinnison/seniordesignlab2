6b6ed513d2a7bf727ef7eb695d9efe6a5e423e0c
EmmaClaire <emma.kinnison@marquette.edu>
2022-02-24 00:12:44 -0600

    It all works, I tested it too

--- lab2/kinnisone_k_means.cpp
+++ lab2/kinnisone_k_means.cpp
@@ -9,6 +9,7 @@
 #include <iterator>
 #include <vector>
 std::ifstream myfile;
+std::ifstream myfile2;
 
 /*
 This sets up all the variables so they can be modified in this file
@@ -21,7 +22,7 @@ meanie::cluster::cluster(){
 }
 
 void meanie::cluster::set_cluster_mean(float data){
-    
+    this->mean=data;
 }
 
 float meanie::cluster::get_cluster_mean(){
@@ -58,16 +59,22 @@ int main(int argc, char** argv){
     meanie::cluster expressed_cluster;
 
     float suppressed_cluser_mean= -0.5;
-    float stationary_cluser_mean= 0;
-    float expressed_cluser_mean= 0.5;
+    float stationary_cluser_mean=  0;
+    float expressed_cluser_mean=  0.5;
 
     std::string lograt_file;
-    lograt_file="log_ratio_input.dat";
+    std::string gene_file;
+    gene_file="gene_list.txt";
+    lograt_file="IR.dat";
     myfile.open(lograt_file);
-    if(myfile.is_open()){
+    myfile2.open(gene_file);
+
+    if((myfile.is_open())&&(myfile2.is_open())){
         
         std::istream_iterator<float> start(myfile), end;    //get the red data for file one
         std::vector<float> data(start, end);
+        std::istream_iterator<std::string> start2(myfile2), end2;    //get the red data for file one
+        std::vector<std::string> data2(start2, end2);
 
         float suppressed_distance;
         float stationary_distance;
@@ -80,58 +87,76 @@ int main(int argc, char** argv){
         std::vector<float> cluster1;
         std::vector<float> cluster2;
         std::vector<float> cluster3;
+        std::vector<float> expressed;
+        std::vector<float> stationary;
+        std::vector<float> suppressed;
+       // printf("initialized\n");
 
         while(criteria>0.0001){ 
+         //   printf("got here\n");
+
             cluster1.clear();
             cluster2.clear();
             cluster3.clear();
-
+            expressed.clear();
+            stationary.clear();
+            suppressed.clear();
+          
             //loop through each cluster
             for(int j=0;j<data.size();j++){     //tab through all the data to sort it
-                for(int i=0;i <3; i++){ //find the distances between clusters
-                    if(i=0){
+                       
+
                         suppressed_cluster.set_cluster_mean(suppressed_cluser_mean);
                         suppressed_cluster.set_distance(data[j]);
                         suppressed_distance=suppressed_cluster.get_distance();
-                    }
-                    else if(i=1){
+                    
+                      
                         stationary_cluster.set_cluster_mean(stationary_cluser_mean);
                         stationary_cluster.set_distance(data[j]);
                         stationary_distance=stationary_cluster.get_distance();
 
-                    }
-                    else if(i=2){
+                         
                         expressed_cluster.set_cluster_mean(expressed_cluser_mean);
                         expressed_cluster.set_distance(data[j]);
                         expressed_distance=expressed_cluster.get_distance();
+                    
 
-                    }
-                    else{
-                        printf("check your loops\n");
-                    }
-                }
-                if(j=0){
                     if((suppressed_distance<stationary_distance)&&(suppressed_distance<expressed_distance)){
-                        cluster1.insert(cluster1.begin(), data[j]);
-                    }
-                    else if((stationary_distance<=suppressed_distance)&&(expressed_distance<=suppressed_distance)){
-                        cluster2.insert(cluster2.begin(), data[j]);
-                    }
-                    else{
-                        cluster3.insert(cluster3.begin(), data[j]);
-                    }                
-                }
-                else{
-                    if((suppressed_distance<stationary_distance)&&(suppressed_distance<expressed_distance)){
-                        vector<float>::iterator testing1= cluster1.insert(cluster1.end(), 1, data[j]);    //load the bucketvals with the array values
+                        if(cluster1.size()==0){
+                            cluster1.insert(cluster1.begin(), data[j]);
+                            suppressed.insert(suppressed.begin(),j);
+                            //printf("here7\n");
+                        }
+                        else{
+                            vector<float>::iterator testing1= cluster1.insert(cluster1.end(), 1, data[j]);    //load the bucketvals with the array values
+                            vector<float>::iterator testing4= suppressed.insert(suppressed.end(), 1, j);    //load the bucketvals with the array values
+
+                        }
                     }
                     else if((stationary_distance<=suppressed_distance)&&(stationary_distance<=expressed_distance)){
-                        vector<float>::iterator testing2= cluster2.insert(cluster2.end(), 1, data[j]);    //load the bucketvals with the array values
+                        if(cluster2.size()==0){
+                            cluster2.insert(cluster2.begin(), data[j]);
+                            stationary.insert(stationary.begin(),j);
+                            //printf("here8\n");
+                        }
+                        else{
+                            vector<float>::iterator testing2= cluster2.insert(cluster2.end(), 1, data[j]);    //load the bucketvals with the array values
+                            vector<float>::iterator testing5= stationary.insert(stationary.end(), 1, j);    //load the bucketvals with the array values
+
+                        }
                     }
                     else{
-                        vector<float>::iterator testing3= cluster3.insert(cluster3.end(), 1, data[j]);    //load the bucketvals with the array values
+                        if(cluster3.size()==0){
+                            cluster3.insert(cluster3.begin(), data[j]);
+                            expressed.insert(expressed.begin(),j);                            
+                            //printf("here8\n");
+                        }
+                        else{
+                            vector<float>::iterator testing3= cluster3.insert(cluster3.end(), 1, data[j]);    //load the bucketvals with the array values
+                            vector<float>::iterator testing6= expressed.insert(expressed.end(), 1, j);    //load the bucketvals with the array values
+                        }
                     }  
-                }
+                
             }
             
             //calc new means
@@ -142,24 +167,52 @@ int main(int argc, char** argv){
             calc.set_mean(cluster3);
             new_clust_mean3=calc.get_mean();
 
-            printf("New mean 1: %f",new_clust_mean1 );
-            printf("New mean 2: %f",new_clust_mean2 );
-            printf("New mean 3: %f",new_clust_mean3 );
+            //printf("New mean 1: %f\n",new_clust_mean1 );
+            //printf("New mean 2: %f\n",new_clust_mean2 );
+            //printf("New mean 3: %f\n",new_clust_mean3 );
             criteria=abs(suppressed_cluser_mean-new_clust_mean1)+abs(stationary_cluser_mean-new_clust_mean2)+abs(expressed_cluser_mean-new_clust_mean3);
             suppressed_cluser_mean=new_clust_mean1;
             stationary_cluser_mean=new_clust_mean2;
             expressed_cluser_mean=new_clust_mean3;
         }
-        printf("Final mean 1: %f",suppressed_cluser_mean );
-        printf("Final mean 2: %f",stationary_cluser_mean );
-        printf("Final mean 3: %f",expressed_cluser_mean );
+        printf("Final mean 1: %f\n",suppressed_cluser_mean );
+        printf("Final mean 2: %f\n",stationary_cluser_mean );
+        printf("Final mean 3: %f\n",expressed_cluser_mean );
+           
+            expressedfile.open("expressed_genes.txt");
+            suppressedfile.open("suppressed_genes.txt");
+            stationaryfile.open("stationary_genes.txt");
+            int a=0;
+            int b=0;
+            int c=0;
+
+            for (int z=0; z<data.size(); z++){
+                if(z==expressed[a]){
+                    expressedfile<<data2[z]<<"\n";
+                    a++;
+                }
+                if(z==suppressed[b]){
+                    suppressedfile<<data2[z]<<"\n";
+                    b++;
+                }
+                if(z==stationary[c]){
+                    stationaryfile<<data2[z]<<"\n";
+                    c++;
+                }
+            }
+            
+            //expressedfile << expressed_cluser_mean<<"\n";
+            //suppressedfile << suppressed_cluser_mean<<"\n";
+           // stationaryfile << stationary_cluser_mean<<"\n";
+
     }
     else{
         if(!(myfile.is_open())){
-            std::cout<<"Error: log_ratio_input.dat cant be found\n";
+            std::cout<<"Error: IR.dat cant be found\n";
             return 1;
         }
     }
+                printf("done\n");
 
     return 0;
 }
---
+++ lab2/kinnisone_pre_process_test.cpp
@@ -0,0 +1,157 @@
+#include "kinnisone_stats.hpp"
+#include "kinnisone_vector_ops.hpp"
+#include "kinnisone_pre_processing.hpp"
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <vector>
+
+std::ifstream myfile;
+std::ifstream myfile2;
+std::ifstream myfile3;
+std::ifstream myfile4;
+std::ifstream myfile5;
+std::ofstream finalfile;
+
+int main(int argc, char** argv){
+    banana::pie obj;
+    tryme::hell calc;
+    pizza::toppings bacon;
+
+    std::string redcellfile;
+    std::string backgroundredfile;
+    std::string greencellfile;
+    std::string backgroundgreenfile;
+    std::string calidatafile;
+    int genenum;
+
+	if(argv[1]!=NULL){
+        redcellfile =argv[1];
+    }
+    else{
+        std::cout<<"Error: No red cell file\n";
+            return 1;
+    }  
+
+    if(argv[2]!=NULL){
+        backgroundredfile =argv[2];
+    }
+    else{
+        std::cout<<"Error: No red background file\n";
+            return 2;
+    } 
+
+    if(argv[3]!=NULL){
+        greencellfile =argv[3];
+    }
+    else{
+        std::cout<<"Error: No green cell file\n";
+            return 3;
+    } 
+
+    if(argv[4]!=NULL){
+        backgroundgreenfile =argv[4];
+    }
+    else{
+        std::cout<<"Error: No green background file\n";
+            return 4;
+    } 
+
+    if(argv[5]!=NULL){
+        calidatafile =argv[5];
+    }
+    else{
+        std::cout<<"Error: No written file name\n";
+            return 5;
+    } 
+
+    if(argv[6]!=NULL){
+        genenum =atoi(argv[6]);
+    }
+    else{
+        std::cout<<"Error: No number of genes\n";
+            return 6;
+    } 
+
+    myfile.open(redcellfile);                              //open the files
+    myfile2.open(backgroundredfile);                              //open the files
+    myfile3.open(greencellfile);                              //open the files
+    myfile4.open(backgroundgreenfile);                              //open the files
+
+    if((myfile.is_open())&&(myfile2.is_open())&&(myfile3.is_open())&&(myfile4.is_open())){
+        std::istream_iterator<float> start(myfile), end;    //get the red data for file one
+        std::vector<float> data(start, end);
+        std::istream_iterator<float> start2(myfile2), end2; //get the red background data for file two
+        std::vector<float> data2(start2, end2);  
+        std::istream_iterator<float> start3(myfile3), end3;    //get the green data for file one
+        std::vector<float> data3(start3, end3);
+        std::istream_iterator<float> start4(myfile4), end4; //get the green background data for file two
+        std::vector<float> data4(start4, end4); 
+
+
+        if((genenum<=data.size())||(genenum<=data2.size())||(genenum<=data3.size())||(genenum<=data4.size())){
+            obj.set_arr_sub(data, data2);
+            std::vector<float> subred = obj.get_arr_sub();
+            obj.set_arr_sub(data3, data4);
+            std::vector<float> subgreen = obj.get_arr_sub();
+
+            calc.set_mean(subred);
+            float meanred= calc.get_mean();
+
+            calc.set_mean(subgreen);
+            float meangreen= calc.get_mean();
+
+            obj.set_div_arr(subred, meanred);
+            std::vector<float> normred = obj.get_div_arr();
+            obj.set_div_arr(subgreen, meangreen);
+            std::vector<float> normgreen = obj.get_div_arr();
+            
+
+            bacon.set_log_ratio(normred, normgreen);
+            std::vector<float> intense_ratio =bacon.get_log_ratio();
+
+            finalfile.open(calidatafile);
+
+            for (int j=0; j<intense_ratio.size();j++){
+                finalfile << intense_ratio[j]<<"\n";
+            }
+            finalfile.close();
+
+        }
+        else{
+            std::cout<<"Error: Data file too small for number of genes requested\n";
+            return 12;
+        }
+
+
+
+    }
+    else{
+        if(!(myfile.is_open())){
+            std::cout<<"Error: Red cell file didnt open\n";
+            return 7;
+        }
+        else if(myfile.is_open()){
+            std::cout<<"Error: Red cell background file didnt open\n";
+            return 8;
+
+        }
+        else if (myfile.is_open()){
+            std::cout<<"Error: Green cell file didnt open\n";
+            return 9;
+
+        }
+        else if(myfile.is_open()){
+            std::cout<<"Error: Green cell background file didnt open\n";
+            return 10;
+        }
+        else{
+            std::cout<<"Error: Unknown error\n";
+            return 11;
+            
+        }
+    }
+    return 0;
+}
